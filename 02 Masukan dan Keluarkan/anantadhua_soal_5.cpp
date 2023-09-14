a35773bafcd1a59a36402f5d324712e260c5ec24
anantadh <anantadhuaheryanty@gmail.com>
2023-09-14 16:18:34 +0700

    Tugas 2 DDP

---
+++ 02 Masukan dan Keluarkan/anantadhua_soal_5.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string str;
+    getline(cin, str);
+    cout << str << endl;
+
+    string strB;
+    getline(cin, str);
+    cout << str << endl;
+
+    string strC;
+    getline(cin, str);
+    cout << str << endl;
+
+    string strD;
+    getline(cin, str);
+    cout << str << endl;
+
+    string strE;
+    getline(cin, str);
+    cout << str << endl;
+    return 0;
+}
